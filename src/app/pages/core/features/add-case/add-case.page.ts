2d5df895d35fcfcd9e715d556458effb51df8c75
yvfsou <yvsouza764@gmail.com>
2023-08-29 19:18:46 -0300

    Adicionar o LeafLet ao projeto #1

--- src/app/pages/core/features/add-case/add-case.page.ts
+++ src/app/pages/core/features/add-case/add-case.page.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { ToastController, ModalController } from "@ionic/angular";
-import {CaseService} from '../../../../services/cases/case.service';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { nanoid } from 'nanoid'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastController } from "@ionic/angular";
+import { nanoid } from 'nanoid';
+import { CaseService } from '../../../../services/cases/case.service';
+import * as L from 'leaflet';
+
 
 @Component({
   selector: 'app-add-case',
@@ -14,6 +16,11 @@ export class AddCasePage implements OnInit {
   date: string;
   form: FormGroup;
   case: any;
+
+  // MAP
+
+  private map: any;
+  private marker: any;
 
   constructor(private toastController: ToastController,
               private caseService: CaseService,
@@ -45,16 +52,55 @@ export class AddCasePage implements OnInit {
 
     await toast.present();
   }
+  
 
   ngOnInit() {
+
     this.form = this.fb.group({
       nome: [this.case?.name, Validators.required],
       data: [this.case?.datas, Validators.required],
       suspeito: [this.case?.suspeito, Validators.required],
       contato: [this.case?.contato, Validators.required],
       local: [this.case?.local, Validators.required],
+      lat: [''],
+      lng: [''],
       photoURL: [this.case?.photoURL]
     });
+  }
+
+  ionViewDidEnter() {
+    let lat: number;
+    let lng: number;
+    navigator.geolocation.getCurrentPosition(pos => {
+      lat = pos.coords.latitude;
+      lng = pos.coords.longitude;
+      this.intiMap(lat, lng);
+    },
+    err => {
+      this.intiMap(0,0);
+    });
+  }
+
+  intiMap(lat: number, lng: number) {
+    this.map = L.map('map').setView([(lat || 0), (lng || 0)], 13);
+    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+    });
+    tiles.addTo(this.map);
+    this.map.doubleClickZoom.disable(); 
+    this.map.on('dblclick',(e) => this.addMarker(e.latlng.lat, e.latlng.lng));
+  }
+  
+  addMarker(lat: number, lng: number) {
+    if(this.form.value.lat && this.form.value.lng) {
+      this.map.removeLayer(this.marker);
+    }
+    console.log("🚀 ~ file: add-case.page.ts:92 ~ AddCasePage ~ addMarker ~ lat:", lat)
+    console.log("🚀 ~ file: add-case.page.ts:92 ~ AddCasePage ~ addMarker ~ lng:", lng)
+    this.form.controls['lat'].setValue(lat);
+    this.form.controls['lng'].setValue(lng);
+    this.form.updateValueAndValidity();
+    this.marker = L.marker([lat, lng]).addTo(this.map);
   }
 
   openImagePicker(event: any) {
